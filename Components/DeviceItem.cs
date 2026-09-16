c76747e2bc72174381b55c880733fa46204f1de3
marco-zora <marcomaria.zora@cnr.it>
2026-09-16 18:19:13 +0200

    video

---
+++ Components/DeviceItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorPWA.Components
+{
+    public record DeviceItem(
+        string Codice,
+        string Descrizione,
+        string Tipo,
+        string Stato,
+        string DataUpdate,
+        string DataUltimaManutenzione,
+        string VideoUrl
+    );
+    
+}
