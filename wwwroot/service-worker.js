1b2b47964b4d3ad85a23a77f72f0b7bdc90ed4f2
marco-zora <marcomaria.zora@cnr.it>
2026-09-18 18:52:16 +0200

    program

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -1,4 +1,42 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+
+// PERMETTE DI RICHIAMARE LA VERSIONE DEL SERVICE WORKER DAL MAIN THREAD
+VERSION = "1.2.43";
+
+self.addEventListener('message', e =>
+        {
+            if (e.data === 'GET_VERSION') {
+                e.source.postMessage(VERSION);
+            }
+        });
+
+
+
+self.addEventListener('install', event => {
+    self.skipWaiting(); // Forza il nuovo SW a diventare attivo subito
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+/*
+self.addEventListener('fetch', event => {
+    // Sempre rete, nessuna cache
+});
+
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        self.skipWaiting()
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        self.clients.claim()
+    );
+});
+*/
